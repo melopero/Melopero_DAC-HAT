47f6aaf269a39f3e18b97fc93ca22a8677b1e3d2
leoli51 <leoli51@users.noreply.github.com>
2020-09-23 12:28:52 +0200

    errore ho inserito delle cose ne file sbagliato

--- config-dac.sh
+++ config-dac.sh
@@ -2,26 +2,3 @@
 
 sed -i 's/dtparam=audio=on/#dtparam=audio=on/' /boot/config.txt 
 printf "\n# Melopero DAC\ndtoverlay=hifiberry-dacplus\n" >> /boot/config.txt 
-
-
-awk '
-    BEGIN {
-        dacplus=0
-    }
-
-    /^[^#]*dtparam=audio=on/ {
-        print "#dtparam=audio=on"
-    } 
-    /^[^#]*ndtoverlay=hifiberry-dacplus/ {
-        if (dacplus == 1) {
-            next
-        }
-        else {
-            print
-            dacplus=1
-        }
-
-    END {
-        if (dacplus == 0)
-            print "\nMelopero DAC\ndtoverlay=hifiberry-dacplus\n"
-    }'
