6de689054d820c632ce9f6432fc9406efd94a2f1
leoli51 <leo_la_rocca@yahoo.it>
2020-09-23 20:33:49 +0200

    Added final message and tested

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Melopero_DAC-HAT
 
 To configure the DAC-HAT simply type:  
-`wget -O config-dac.sh  https://raw.githubusercontent.com/melopero/Melopero_DAC-HAT/master/config-dac.sh && sudo bash config-dac.sh`
+`wget -q -O config-dac.sh  https://raw.githubusercontent.com/melopero/Melopero_DAC-HAT/master/config-dac.sh && sudo bash config-dac.sh`
--- config-dac.sh
+++ config-dac.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget -O config-dac-awk https://raw.githubusercontent.com/melopero/Melopero_DAC-HAT/master/config-dac-awk
+wget -q -O config-dac-awk https://raw.githubusercontent.com/melopero/Melopero_DAC-HAT/master/config-dac-awk
 awk -f config-dac-awk /boot/config.txt > config.txt.tmp
 cat config.txt.tmp > /boot/config.txt
 rm config-dac-awk config.txt.tmp
 rm config-dac.sh
+echo "Done. Reboot to enable the DAC"
