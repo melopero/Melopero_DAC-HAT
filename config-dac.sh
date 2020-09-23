8dc61d370a929092357dbef51d45beaa93d2d575
leoli51 <leo_la_rocca@yahoo.it>
2020-09-23 20:08:53 +0200

    Testing

--- config-dac.sh
+++ config-dac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/melopero/Melopero_DAC-HAT/master/config-dac-awk > config-dac-awk
+wget -O config-dac-awk https://raw.githubusercontent.com/melopero/Melopero_DAC-HAT/master/config-dac-awk
 awk -f config-dac-awk /boot/config.txt > config.txt.tmp
 cat config.txt.tmp > /boot/config.txt
 rm config-dac-awk config.txt.tmp
+rm config-dac.sh
